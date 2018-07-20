b1fdcbdfcc048139ab68f7c9e12736e9bb0b1d40
berkkurt <berkk1994@hotmail.com>
2018-07-20 15:24:15 +0300

    1.- Cümledeki kelime sayısını buldurur.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KelimeBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle;
+            Console.Write("Cumleyi Girin : ");  // Kullanıcıdan cümle
+            cumle = Console.ReadLine();         // alıyoruz.
+            string[] kelimeler = cumle.Split(' '); // ayırma için split kullanılır.
+            Console.WriteLine(kelimeler.Length);
+            Console.ReadKey();
+        }
+    }
+}
