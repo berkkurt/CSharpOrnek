a29d982a3ef5bed234fed59ce497d4f95a1bc3c0
berkkurt <berkk1994@hotmail.com>
2018-07-23 22:32:31 +0300

    Update 2.-EbobBulma.cs

--- 2.-EbobBulma.cs
+++ 2.-EbobBulma.cs
@@ -10,40 +10,38 @@ namespace EbobBulma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("İlk sayıyı giriniz: ");
+            Console.WriteLine("Ebob'u bulunacak ilk sayıyı giriniz: ");  
             int sayi_1 = Convert.ToInt32(Console.ReadLine());
             
-            Console.WriteLine("İkinci sayıyı giriniz: ");
+            Console.WriteLine("Ebob'u bulunacak ikinci sayıyı giriniz: ");
             int sayi_2 = Convert.ToInt32(Console.ReadLine());
             
             int ebob = 1;
             int bolen = 2;
             while (sayi_1 > 1 || sayi_2 > 1)
             {
-                // Sayilardan her ikiside, bolen 
-                // degiskenine bolundugu takdirde, 
-                // obeb hesabina katilir.
+                // Sayiların ikisi de, bölen degiskenine bolundugunde ebob hesabina katılmış olunur.
+                
                 if (sayi_1 % bolen == 0 && sayi_2 % bolen == 0)
                 {
-                    ebob *= bolen;
-                    sayi_1 /= bolen;
-                    sayi_2 /= bolen;
+                    ebob =ebob * bolen;
+                    sayi_1 =sayi_1 / bolen;
+                    sayi_2 =sayi_2 / bolen;
                 }
                 else if (sayi_1 % bolen == 0)
                 {
-                    sayi_1 /= bolen;
+                    sayi_1 =sayi_1 / bolen;
                 }
                 else if (sayi_2 % bolen == 0)
                 {
-                    sayi_2 /= bolen;
+                    sayi_2 =sayi_2 / bolen;
                 }
                 else
                 {
                     bolen++;
                 }
             }
-            Console.WriteLine("Verilen sayıların ebob'u ; ");
-            Console.WriteLine(ebob);
+            Console.WriteLine("Verilen sayıların ebob'u ; ",ebob);
             Console.ReadKey();
         }
     }
