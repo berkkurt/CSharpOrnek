31a6abd13b27bd02bbab7fdacf502def9820b3db
berkkurt <berkk1994@hotmail.com>
2018-07-25 15:37:24 +0300

    Update 1.-CumledekiKelimeleriBulma.cs
    Kullanıcı aldığı cümledeki kelime sayısını hesaplar. Örneğin; "Cümledeki kelime sayısını bulma programı" girildiğinde çıktı : 5 olacaktır.

--- 1.-CumledekiKelimeleriBulma.cs
+++ 1.-CumledekiKelimeleriBulma.cs
@@ -13,7 +13,7 @@ namespace KelimeBulma
             string cumle;
             Console.Write("Cumleyi Girin : ");  // Kullanıcıdan cümle
             cumle = Console.ReadLine();         // alıyoruz.
-            string[] kelimeler = cumle.Split(' '); // ayırma için split kullanılır.
+            string[] kelimeler = cumle.Split(' '); // Ayırma işlemi için split kullanılır.
             Console.WriteLine(kelimeler.Length);
             Console.ReadKey();
         }
