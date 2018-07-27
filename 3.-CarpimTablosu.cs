5b080f7fd2ef2838a6d3f0cf0a96bb55acd736b1
berkkurt <berkk1994@hotmail.com>
2018-07-27 15:02:45 +0300

    Girilen sayının çarpım tablosu

---
+++ 3.-CarpimTablosu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpimTablosu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----- Girilen sayının çarpım tablosunu bulan program. ----- \n");
+
+            Console.WriteLine("Çarpım tablosu istenen sayıyı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n----- " + sayi + " sayısının çarpım tablosu -----");
+            Console.WriteLine();
+            for (int i = 1; i <= 10 ; i++)
+                Console.WriteLine(sayi + " x " + i + " = " + sayi * i);
+
+            Console.ReadKey();
+        }
+    }
+}
