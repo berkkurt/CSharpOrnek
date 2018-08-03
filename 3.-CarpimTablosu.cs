72a3b112c2bf29ed0e0fe62cde94252fa31320f6
berkkurt <berkk1994@hotmail.com>
2018-08-03 17:12:11 +0300

    Update 3.-CarpimTablosu.cs

--- 3.-CarpimTablosu.cs
+++ 3.-CarpimTablosu.cs
@@ -13,12 +13,12 @@ namespace CarpimTablosu
             Console.WriteLine("----- Girilen sayının çarpım tablosunu bulan program. ----- \n");
 
             Console.WriteLine("Çarpım tablosu istenen sayıyı giriniz : ");
-            int sayi = int.Parse(Console.ReadLine());
+            int sayi = int.Parse(Console.ReadLine());  // Girilen sayi,sayi adlı değişkene atandı.
             
             Console.WriteLine("\n----- " + sayi + " sayısının çarpım tablosu -----\n");
             
             for (int i = 1; i <= 10 ; i++)
-                Console.WriteLine(sayi + " x " + i + " = " + sayi * i);
+                Console.WriteLine(sayi + " x " + i + " = " + sayi * i);  // Çarpım tablosunun ekrana yazdırıldığı kısım.
 
             Console.ReadKey();
         }
