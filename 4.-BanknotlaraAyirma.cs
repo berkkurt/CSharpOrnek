abcac95c98ba0f9ab46a14570dfc349722be80aa
berkkurt <berkk1994@hotmail.com>
2018-08-05 16:38:39 +0300

    Girilen parayı banknotlara ayırır.

---
+++ 4.-BanknotlaraAyirma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanknotlaraAyirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] banknot = { 200, 100, 50, 20, 10, 5, 1 };
+            int para, adet, i;
+            Console.Write("Ödenecek tutar girin : ");
+            para = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("==========================");
+            for (i = 0; i <= 6; i++)
+            {
+              adet = para / banknot[i];
+              if (adet != 0)
+                Console.WriteLine(adet.ToString() + " adet : " + Convert.ToString(banknot[i] + " TL "));
+              para %= banknot[i];
+            }
+            Console.ReadKey();
+        }
+    }
+}
