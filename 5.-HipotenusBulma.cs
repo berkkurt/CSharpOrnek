872f054093276f281319837ab67f82659954e7cc
berkkurt <berkk1994@hotmail.com>
2018-08-07 19:45:46 +0300

    Update 5.-HipotenusBulma.cs

--- 5.-HipotenusBulma.cs
+++ 5.-HipotenusBulma.cs
@@ -12,16 +12,19 @@ namespace HipotenusBulma
         {
 
             double k1, k2, hipotenus;
+            
             Console.Write("Birinci Dik Kenarı Girin....:");
             k1 = Convert.ToDouble(Console.ReadLine());
+            
             Console.Write("İkinci Dik Kenarı Girin....:");
             k2 = Convert.ToDouble(Console.ReadLine());
+            
             //k1 ve k2nin karelerini toplamını yapıp kare kökünü alıyoruz.
             hipotenus = Math.Sqrt((k1 * k1) + (k2 * k2));
+           
             Console.WriteLine("Hipotenüs değeri....:{0}", hipotenus);
             Console.ReadKey();
 
-            // Dik kenar uzunlukları verilen dik üçgende hipotenüs hesaplayan örnek:
 
 
         }
