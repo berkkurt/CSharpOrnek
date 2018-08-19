ab2b89a6ffa58249a350aa951a8747fa44a96cf3
berkkurt <berkk1994@hotmail.com>
2018-08-19 16:13:56 +0300

    Us alma işlemini pow'suz olarak yapar.

---
+++ 6.UsAlmaPowsuz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsAlmaPowsuz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taban, us, sonuc = 1;
+            Console.Write("Taban sayısı : ");
+            taban = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kuvveti : ");
+            us = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= us; i++)
+            {
+                sonuc = sonuc * taban;
+            }
+
+            Console.WriteLine("Tabanı {0} ve kuvveti {1} olan sayının değeri = {2}", taban, us, sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
