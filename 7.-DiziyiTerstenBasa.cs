f9de52873cbfc4160028129d2f7b57c8a5b6be39
berkkurt <berkk1994@hotmail.com>
2018-08-25 17:20:58 +0300

    Dizideki elemanları tersten başa doğru yazdırır.

---
+++ 7.-DiziyiTerstenBasa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziyiTerstenBasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strDizi = { "Ali", "Buğra", "Gözde", "Hakan", "Seda" };
+            int son = strDizi.Length - 1;
+            for (int i = son; i >= 0; --i)
+            {
+                Console.WriteLine(strDizi[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
