3e293eefc995b101bb0ffb48321d41fe73937ae7
berkkurt <berkk1994@hotmail.com>
2018-10-01 16:12:24 +0300

    Update 10.-AsiriYuklemeOrnegi.cs

--- 10.-AsiriYuklemeOrnegi.cs
+++ 10.-AsiriYuklemeOrnegi.cs
@@ -4,38 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// C# Metotları Aşırı Yükleme (Overloading) örneğidir.
+
 namespace ConsoleApplication8
 {
     class Program
     {
-        static int ortalama(int yazili1, int yazili2)
+        static int ortalama(int yazili1, int yazili2) // İlk kullanımında metodumuz 2 değer alıyor.
         {
             int sonuc = (yazili1 + yazili2) / 2;
             return sonuc;
         }
 
 
-        static int ortalama(int yazili1, int yazili2, int sozlu)
-        {
+        static int ortalama(int yazili1, int yazili2, int sozlu) // İkinci kullanımında metodumuz artık 3 değer alıyor.
+        {                                                        // Metodumuza ikinci bir kullanım katarak aşırı yükledik.
             int sonuc = (yazili1 + yazili2 + sozlu) / 3;
             return sonuc;
         }
 
-        static int ortalama(int yazili1, int yazili2, int sozlu, int odev)
-        {
+        static int ortalama(int yazili1, int yazili2, int sozlu, int odev) // Üçüncü kullanımında ise metodumuz 4 değer alıyor.
+        {                                                                  //Metodumuza son olarak bir kullanım daha ekleyip aşırı yüklemiş olduk.
             int sonuc = (yazili1 + yazili2 + sozlu + odev) / 4;
             return sonuc;
         }
 
-
-
-
         static void Main(string[] args)
         {
 
-            Console.WriteLine(ortalama(50, 60));
-            Console.WriteLine(ortalama(50, 80, 90));
-            Console.WriteLine(ortalama(50, 40, 90, 100));
+            Console.WriteLine(ortalama(100, 100));
+            Console.WriteLine(ortalama(100, 100, 100));
+            Console.WriteLine(ortalama(100, 100, 100, 100));
 
             Console.ReadKey();
         }
@@ -44,12 +43,3 @@ namespace ConsoleApplication8
 
     }
 }
-
-
-/* C# Metotları Aşırı Yükleme (Overloading) örneğidir.Metodun 3 kullanımı vardır. 
-Burada ortalama metodumuz; 
- 1-hem 2 yazılı,
- 2-hem 2 yazılı ve 1 sözlü 
- 3-hem de 2 yazılı,1 sözlü ve 1 ödev notu 
-    değerlerini alabilmektedir.
-*/
